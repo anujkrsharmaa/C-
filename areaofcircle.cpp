8666f58b69a807c7a134de95805252b6354344f7
Anuj kr Sharma <188915825+anujkrsharmaa@users.noreply.github.com>
2025-07-30 22:04:54 +0530

    basics

---
+++ areaofcircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float r = 4.3;
+    float a = 3.141592 * r * r; // Area of circle formula: πr²
+    cout << "The area of the circle is: " << a << endl; // Output the area
+    return 0; // Indicate that the program ended successfully
+
+}
---
+++ arithmeticOperators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 10 , y = 5;
+    cout<<x + y << endl; // Addition
+    cout<<x - y << endl; // Subtraction     
+    cout<<x * y << endl; // Multiplication
+    cout<<x / y << endl; // Division
+    cout<<x % y << endl; // Modulus
+    // Increment and Decrement Operators
+    cout<<x++ << endl; // Post-increment
+    cout<<++x << endl; // Pre-increment
+    cout<<x-- << endl; // Post-decrement
+    cout<<--x << endl; // Pre-decrement
+    cout<<y++ << endl; // Post-increment on y
+    cout<<++y << endl; // Pre-increment on y
+    cout<<y-- << endl; // Post-decrement on y
+    cout<<--y << endl; // Pre-decrement on y
+    return 0; // Indicate that the program ended successfully       
+}
